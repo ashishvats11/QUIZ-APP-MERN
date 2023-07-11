82c6989a2f1c5c7818d6cd8852631f0fd926bdcf
Ashish Vats <80643755+ashishvats11@users.noreply.github.com>
2023-07-11 23:39:21 +0530

    final commit

--- src/api/axios.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-const port = 8000
-
-export default axios.create({
-    baseURL:`http://localhost:${port}`
-});
--- src/api/routes/test.js
+++
@@ -1,8 +0,0 @@
-import Express from 'express';
-const router = Express.Router();
-
-router.get('/', (req, res, next) => {
-    res.status(200).json({ message: 'api working' });
-})
-
-export default router;
