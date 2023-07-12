7e69b44090c4d0cfbf40974ec86ac7f787e2f21c
Mukund Kumar <banananarick69@gmail.com>
2023-07-13 02:25:34 +0530

    remote api connection

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const port = 8000
+// const port = 8000
 
 export default axios.create({
-    baseURL:`http://localhost:${port}`
+    baseURL:`https://quiz-app-mern-backend.vercel.app/`
 });
